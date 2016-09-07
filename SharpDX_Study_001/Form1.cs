5b3b53c743709670e1d71150ac0fbb3cd61de91a
sun xin <darksun190@hotmail.com>
2016-09-07 10:45:36 +0800

    简单框架

--- SharpDX_Study_001/Form1.cs
+++ SharpDX_Study_001/Form1.cs
@@ -8,14 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SharpDX.Windows;
+using Device = SharpDX.Direct3D11.Device;
+
 
 namespace SharpDX_Study_001
 {
     public partial class Form1 : RenderForm
     {
+        Device device;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            device = new Device(SharpDX.Direct3D.DriverType.Hardware);
+            RenderLoop.Run(this, Draw3D);
+        }
+
+        private void Draw3D()
+        {
+            
         }
     }
 }
