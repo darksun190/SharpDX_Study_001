049d8e4a7ad95088a62d0d9a805e4dca75d728d0
sun xin <darksun190@hotmail.com>
2016-09-14 10:11:18 +0800

    之前的bug都是由device引起的,createwithswapchain已经out引用了device，再实例一次导致两边对不上，后面全乱。

--- SharpDX_Study_001/Form1.cs
+++ SharpDX_Study_001/Form1.cs
@@ -11,8 +11,10 @@ using SharpDX;
 using SharpDX.DXGI;
 using SharpDX.Direct3D11;
 using SharpDX.Direct3D;
+using SharpDX.D3DCompiler;
 
 using Device = SharpDX.Direct3D11.Device;
+using Buffer = SharpDX.Direct3D11.Buffer;
 
 
 namespace SharpDX_Study_001
@@ -43,16 +45,67 @@ namespace SharpDX_Study_001
                 SwapEffect = SwapEffect.Discard,
                 Usage = Usage.RenderTargetOutput
             };
-            Device.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.BgraSupport, new SharpDX.Direct3D.FeatureLevel[] { SharpDX.Direct3D.FeatureLevel.Level_11_0 }, desc, out device, out swapChain);
+            Device.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.Debug, new FeatureLevel[] { FeatureLevel.Level_11_0 }, desc, out device, out swapChain);
+            //System.Diagnostics.Debug.Assert(device.CheckMultisampleQualityLevels(Format.R8G8B8A8_UNorm, 4) > 0);
             context = device.ImmediateContext;
-            Texture2D backBuffer = Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+            Texture2D backBuffer = SharpDX.Direct3D11.Resource.FromSwapChain<Texture2D>(swapChain, 0);
             renderView = new RenderTargetView(device, backBuffer);
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            device = new Device(SharpDX.Direct3D.DriverType.Hardware);
+
+            int[] indices_array = new int[]
+           {
+                0,1,2,0,2,3,
+                4,6,5,4,7,6,
+                8,9,10,8,10,11,
+                12,14,13,12,15,14,
+                16,18,17,16,19,18,
+                20,21,22,20,22,23
+           };
+            var indices = Buffer.Create(device, BindFlags.IndexBuffer, indices_array);
+
+            var vertices = Buffer.Create(device, BindFlags.VertexBuffer, new[]
+                                           {
+                                      new Vector4(0.0f, 0.5f, 0.5f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                                      new Vector4(0.5f, -0.5f, 0.5f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4(-0.5f, -0.5f, 0.5f, 1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f)
+                                  });
+            // Ignore all windows events
+            var factory = swapChain.GetParent<Factory>();
+            factory.MakeWindowAssociation(Handle, WindowAssociationFlags.IgnoreAll);
+            // New RenderTargetView from the backbuffer
+            var backBuffer = Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+            var renderView = new RenderTargetView(device, backBuffer);
+
+            // Create Constant Buffer
+            var contantBuffer = new Buffer(device, Utilities.SizeOf<Matrix>(), ResourceUsage.Default, BindFlags.ConstantBuffer, CpuAccessFlags.None, ResourceOptionFlags.None, 0);
+            // Compile Vertex and Pixel shaders
+            var vertexShaderByteCode = ShaderBytecode.CompileFromFile("shader.fx", "VS", "vs_4_0");
+            var vertexShader = new VertexShader(device, vertexShaderByteCode);
+
+            var pixelShaderByteCode = ShaderBytecode.CompileFromFile("shader.fx", "PS", "ps_4_0");
+            var pixelShader = new PixelShader(device, pixelShaderByteCode);
+
+            var signature = ShaderSignature.GetInputSignature(vertexShaderByteCode);
+            // Layout from VertexShader input signature
+            var layout = new InputLayout(device, signature, new[]
+                    {
+                        new InputElement("POSITION", 0, Format.R32G32B32A32_Float, 0, 0),
+                        new InputElement("COLOR", 0, Format.R32G32B32A32_Float, 16, 0)
+                    });
+            // Prepare All the stages
+
+            context.InputAssembler.InputLayout = layout;
+            context.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+            context.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(vertices, Utilities.SizeOf<Vector4>() * 2, 0));
+            context.VertexShader.SetConstantBuffer(0, contantBuffer);
+            context.VertexShader.Set(vertexShader);
+            context.PixelShader.Set(pixelShader);
+
+
             RenderLoop.Run(this, Draw3D);
         }
 
